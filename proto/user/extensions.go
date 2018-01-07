7ba26776615a05bfc97190a726612db57c0e6669
Jake Bjorke <jrbjorke@gmail.com>
2018-01-07 00:04:08 -0600

    Upaded for latest uuid interface

--- proto/user/extensions.go
+++ proto/user/extensions.go
@@ -8,6 +8,10 @@ import (
 //BeforeCreate is used to generate a UUID for the user ID rather than an
 //autoincrementing integer.
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid := uuid.NewV4()
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+
 	return scope.SetColumn("Id", uuid.String())
 }
